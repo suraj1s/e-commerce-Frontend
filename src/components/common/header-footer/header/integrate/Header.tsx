e04751a16c192d7369b154001e656edb4cbd9519
suraj subedi <surajsubedi059@gmail.com>
2024-01-02 21:27:15 +0545

    menue  basic layout created

--- src/components/common/header-footer/header/integrate/Header.tsx
+++ src/components/common/header-footer/header/integrate/Header.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Header = () => {
 
   return (
-    <div className=' flex flex-row gap-x-5 flex-shrink-0 container p-3 '>
+    <div className=' flex flex-row gap-x-5 flex-shrink-0 container p-3  font-semibold text-gray-800 capitalize'>
       <Image  src={Logo} alt='logo' className='h-10 w-10'/>
       <p>welcome</p>
       <Search  placeholder='Search Product'  />
--- src/components/common/header-footer/header/integrate/Navbar.tsx
+++ src/components/common/header-footer/header/integrate/Navbar.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex container gap-x-5 font-semibold text-gray-800 capitalize'>
+      <div>
+        all
+      </div>
+      <div>
+        todays deals
+      </div>
+      <div>
+        registry
+      </div>
+      <div>
+        most popular products
+      </div>
+    </div>
   )
 }
 
