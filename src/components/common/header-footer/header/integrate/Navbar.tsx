4bd38c694b9f8f9978be51ee500cae5dc378f8f2
suraj subedi <surajsubedi059@gmail.com>
2024-01-05 20:59:19 +0545

    desg : side navebar layout created

--- src/components/common/header-footer/header/integrate/Navbar.tsx
+++ src/components/common/header-footer/header/integrate/Navbar.tsx
@@ -22,19 +22,21 @@ const Navbar = () => {
   return (
     <div className='bg-gray-700 text-gray-300'>
     <div className='flex container gap-x-5 font-semibold flex-wrap justify-start py-3 capitalize'>
-      <div className='flex gap-x-2  items-center hover:cursor-pointer'>
-        all
-        <div className='flex flex-col font-bold text-lg leading-[5px] -tracking-[3px]'>
-          <p>--</p>
-          <p>--</p>
-          <p>--</p>
+      <div className='flex gap-x-2  items-center hover:cursor-pointer' onClick={()=> setIsSideNavbarOpen((prev) => !prev)}>
+        
+        <div className='flex flex-col  font-light  text-xl leading-[5px] -tracking-[5px]'>
+          <p>---</p>
+          <p>---</p>
+          <p>---</p>
         </div>
+        All
       </div>
       {
         isSideNavbarOpen && 
-      <div className='relative '>
-      <SideNavbar  />
-      </div>
+        <div>
+          <SideNavbar handelClose = {setIsSideNavbarOpen}  />
+
+        </div>
       }
      {
      navItems.map((item, index) => (
--- src/components/common/header-footer/header/integrate/SideNavbar.tsx
+++ src/components/common/header-footer/header/integrate/SideNavbar.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
-const SideNavbar = () => {
+interface SideNavbarProp {
+    handelClose : React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SideNavbar = ({handelClose} : SideNavbarProp) => {
     const sideNavbarItems  = [
         {
             mainTitle : "Shop By Department",
@@ -9,24 +14,24 @@ const SideNavbar = () => {
                     itemTitle : "Electronics",
                     subItems : [
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                     ]
                 },
@@ -34,24 +39,24 @@ const SideNavbar = () => {
                     itemTitle : "Fashon",
                     subItems : [
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                         {
-                            "name " : "Mobiles",
-                            "link" : "/"
+                            name  : "Mobiles",
+                            link : "/"
                         },
                     ]
                 },
@@ -60,7 +65,50 @@ const SideNavbar = () => {
        
     ]
   return (
-    <div>SideNavbar</div>
+    <div className='relative  '>
+    <div className='fixed left-0 top-0   h-screen w-80 bg-gray-300 text-gray-800 z-20 '>
+        <div className='bg-gray-800 text-gray-300 font-bold text-xl  py-5 px-8'>
+            hello User
+        </div>
+        <div className='px-8 flex flex-col gap-y-4 '>
+        {
+            sideNavbarItems.map((item , index) => (
+                <div key={index} className='flex flex-col gap-y-2'>
+                    <div>
+                        {item.mainTitle}
+                    </div>
+                    <div>
+                        {
+                            item?.items.map( (item , index) => (
+                                <div key={index}>
+                                    <div>
+                                        {item.itemTitle}
+                                    </div>
+                                    <div>
+                                        {
+                                            item?.subItems.map((item , index) => (
+                                                <Link href={item.link} key={index} >
+                                                    <p>
+                                                        {item.name}
+                                                    </p>
+                                                </Link>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                </div>
+            )) 
+        }
+
+        </div>
+    </div>
+    <div></div>
+    <div  className='h-screen w-screen  fixed top-0 left-0 popupmodalblur z-10'  onClick={()=> handelClose(false)}/> 
+    </div>
   )
 }
 
