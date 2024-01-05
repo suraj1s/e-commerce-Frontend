6db6e06b438d1448f730c7f1970a45435d896f49
suraj subedi <surajsubedi059@gmail.com>
2024-01-05 17:00:19 +0545

    desg: working on side navbar

--- src/components/common/header-footer/header/integrate/Navbar.tsx
+++ src/components/common/header-footer/header/integrate/Navbar.tsx
@@ -1,21 +1,49 @@
-import React from 'react'
+"use client"
+import Link from 'next/link'
+import React, { useState } from 'react'
+import SideNavbar from './SideNavbar'
 
 const Navbar = () => {
+  const navItems = [
+    {
+      name : "Today's Deals",
+      link : "/",
+    },
+    {
+      name : "Limited Offers",
+      link : "/",
+    },
+    {
+      name : "Most Popular Products",
+      link : "/",
+    },
+  ]
+  const [isSideNavbarOpen, setIsSideNavbarOpen] = useState(false)
   return (
     <div className='bg-gray-700 text-gray-300'>
     <div className='flex container gap-x-5 font-semibold flex-wrap justify-start py-3 capitalize'>
-      <div>
+      <div className='flex gap-x-2  items-center hover:cursor-pointer'>
         all
+        <div className='flex flex-col font-bold text-lg leading-[5px] -tracking-[3px]'>
+          <p>--</p>
+          <p>--</p>
+          <p>--</p>
+        </div>
       </div>
-      <div>
-        todays deals
+      {
+        isSideNavbarOpen && 
+      <div className='relative '>
+      <SideNavbar  />
       </div>
-      <div>
-        registry
+      }
+     {
+     navItems.map((item, index) => (
+      <div key={index} className='hover:cursor-pointer'>
+        <Link href={item.link} >
+        {item.name}
+        </Link>
       </div>
-      <div>
-        most popular products
-      </div>
+     ))}
     </div>
 
     </div>
