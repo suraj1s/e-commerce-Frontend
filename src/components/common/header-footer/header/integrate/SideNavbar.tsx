a2a870aa47f6386c86cdfbc3158d6aee97f297de
suraj subedi <surajsubedi059@gmail.com>
2024-01-05 17:07:27 +0545

    desg: side navbar items created

--- src/components/common/header-footer/header/integrate/SideNavbar.tsx
+++ src/components/common/header-footer/header/integrate/SideNavbar.tsx
@@ -1,6 +1,64 @@
 import React from 'react'
 
 const SideNavbar = () => {
+    const sideNavbarItems  = [
+        {
+            mainTitle : "Shop By Department",
+            items : [
+                {
+                    itemTitle : "Electronics",
+                    subItems : [
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                    ]
+                },
+                {
+                    itemTitle : "Fashon",
+                    subItems : [
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                        {
+                            "name " : "Mobiles",
+                            "link" : "/"
+                        },
+                    ]
+                },
+            ]
+        },
+       
+    ]
   return (
     <div>SideNavbar</div>
   )
