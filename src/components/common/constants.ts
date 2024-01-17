b2e5a4d011695117b7323780883f2d736494e148
suraj subedi <surajsubedi059@gmail.com>
2024-01-17 18:37:35 +0545

    desg: cart page designing

---
+++ src/components/common/constants.ts
@@ -0,0 +1,45 @@
+export const cartData = [
+    {
+      "title": "iPhone 9",
+      "description": "An apple mobile which is nothing like apple",
+      "price": 993,
+      "discountPercentage": 12.96,
+      "rating": 0,
+      "stock": 0,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://cdn.dummyjson.com/data/products/1/thumbnail.jpg",
+      "images": [
+        "https://cdn.dummyjson.com/data/products/1/1.jpg",
+        "https://cdn.dummyjson.com/data/products/1/2.jpg",
+        "https://cdn.dummyjson.com/data/products/1/3.jpg",
+        "https://cdn.dummyjson.com/data/products/1/thumbnail.jpg"
+      ],
+      "deleted": true,
+      "delete": true,
+      "quantity": 2,
+      "user": 2,
+      "productId": 1,
+      "id": 1
+    },
+    {
+      "title": "OPPOF19",
+      "description": "OPPO F19 is officially announced on April 2021.",
+      "price": 2217,
+      "discountPercentage": 17.91,
+      "rating": 0,
+      "stock": 123,
+      "brand": "OPPO",
+      "category": "selectone",
+      "thumbnail": "https://cdn.dummyjson.com/data/products/4/thumbnail.jpg",
+      "images": [
+        "https://cdn.dummyjson.com/data/products/4/1.jpg",
+        "https://cdn.dummyjson.com/data/products/4/2.jpg",
+        "https://cdn.dummyjson.com/data/products/4/3.jpg"
+      ],
+      "quantity": 3,
+      "user": 2,
+      "productId": 8,
+      "id": 2
+    }
+  ]
