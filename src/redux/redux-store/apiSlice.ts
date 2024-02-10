cf7bd9e3fae540f6275aa2276a510d15d7d662e3
suraj subedi <surajsubedi059@gmail.com>
2024-02-10 22:18:29 +0545

    auth token added to headers

--- src/redux/redux-store/apiSlice.ts
+++ src/redux/redux-store/apiSlice.ts
@@ -1,18 +1,62 @@
 import {
+  BaseQueryApi,
+  FetchArgs,
   createApi,
   fetchBaseQuery
 } from "@reduxjs/toolkit/query/react"
+import Cookies from "js-cookie"
+
 
 // export const backendurl = process.env.BACKEND_URL || "https://dummyjson.com/"
 export const backendurl = process.env.BACKEND_URL || "http://localhost:8000/api/v1/"
-// const backendurl = "http://192.168.1.131:8000/"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: backendurl,
+  // here we are preparing the headers that need to be sent with each request
+  prepareHeaders : (headers ) => {
+    const access_token = Cookies.get("access_token")
+    if(access_token) {
+      headers.set("Authorization", `Bearer ${access_token}`)
+    }
+    return headers
+  }
 })
-export const apiSlice = createApi({
+const baseQueryWithReauth = async (
+  args : string | FetchArgs , 
+  api : BaseQueryApi ,
+  extraOptions : {},
+) => {
+const result = await baseQuery(args, api, extraOptions);
+// if the auth token is expired, we will try to refresh it using the refress token and then set it in headers and send the request again
+if(result.error && result.error.status === 401) {
+  const refresh_token = Cookies.get("refresh_token")
+  if(refresh_token) {
+    const refreshResult : any = await baseQuery( 
+       {
+      url : "auth/token/refresh/",
+      method: "POST",
+      body: {refresh: refresh_token}
+    },
+    api , 
+    extraOptions
+    )
+    console.log(refreshResult , "refreshResult")
+    if(refreshResult.data) {
+      Cookies.set("access_token", refreshResult.data.access)
+      const result =  baseQuery(args, api, extraOptions);
+      return result
+    }
+    else {
+      Cookies.remove("access_token");
+      Cookies.remove("refresh_token");
+    }
+  }
+}
+return result
+}
+ export const apiSlice = createApi({
   reducerPath: "apiSlice",
-  baseQuery,
+  baseQuery : baseQueryWithReauth,
   tagTypes: [
     "authUser",
     "forgotPassword",
