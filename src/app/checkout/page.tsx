625ea4f1b3c3994493d7765707f828646154a952
suraj subedi <surajsubedi059@gmail.com>
2024-02-16 21:42:43 +0545

    minor adjustments

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,18 +1,23 @@
 import CheckoutAddressDetail from '@/components/checkout/CheckoutAddressDetail'
 import CheckoutCartDetail from '@/components/checkout/CheckoutCartDetail'
 import CheckoutPaymentDetail from '@/components/checkout/CheckoutPaymentDetail'
+import CustomButton from '@/components/common/custom/CustomButton'
 import React from 'react'
 
 const page = () => {
   return (
+    <div className=''>
     <div className='flex flex-col lg:flex-row space-x-10 space-y-8 pt-10  container justify-center '>
       <div>
       <CheckoutAddressDetail />
-      <CheckoutPaymentDetail />
+      {/* <CheckoutPaymentDetail /> */}
       </div>
       <div >
         <CheckoutCartDetail />
       </div>
+    </div>
+
+    <CustomButton title='Place Order' className='!w-fit mx-auto my-10' />
     </div>
   )
 }
